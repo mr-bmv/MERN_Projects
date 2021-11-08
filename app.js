4f5da733791b958fac62a05a8d7f816bb0a360ab
Mike Botkin <mr.m.botkin@gmail.com>
2021-11-08 11:56:14 +0300

    FEAT: server configuration

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Start")
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express();
+const PORT = config.get('port') || 5000
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+    })
+    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+  } catch (e) {
+    console.log('Server Error', e.message)
+    process.exit(1)
+  }
+}
+
+start()
